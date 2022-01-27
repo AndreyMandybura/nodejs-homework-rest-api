12c9a0b1c25906f9501bd2664cb21383e0e3ba7e
AndreyMandybura <80692584+AndreyMandybura@users.noreply.github.com>
2022-01-27 18:19:52 +0200

    MongoDB, Mongoose, dotenv

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const app = require('./app')
+const mongoose = require('mongoose');
+
+const dotenv = require('dotenv');
+dotenv.config()
 
 const {PORT = 3000} = process.env
 
@@ -6,10 +10,10 @@ app.listen(PORT, () => {
   console.log(`Server running. Use our API on port: ${PORT}`)
 })
 
-const mongoose = require('mongoose');
-
 const { DB_HOST } = process.env;
 
 mongoose.connect(DB_HOST)
   .then(() => console.log('data base connect success'))
-.catch(error => console.log(error.message))
+  .catch(error => console.log(error.message))
+
+
