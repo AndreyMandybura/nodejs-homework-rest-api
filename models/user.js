18c2d6e80e4eb5593b3e29a8b2e884e143c1b622
AndreyMandybura <80692584+AndreyMandybura@users.noreply.github.com>
2022-02-27 19:29:37 +0200

    Верификация email

--- models/user.js
+++ models/user.js
@@ -27,6 +27,14 @@ const userSchema = Schema({
     avatarURL: {
         type: String,
     },
+     verify: {
+    type: Boolean,
+    default: false,
+  },
+  verificationToken: {
+    type: String,
+    required: [true, 'Verify token is required'],
+  },
 }, { versionKey: false, timestamps: true });
 
 const registerJoiSchema = Joi.object({
@@ -34,10 +42,15 @@ const registerJoiSchema = Joi.object({
     password: Joi.string().min(6).required(),
 })
 
+const verifyEmailschema = Joi.object({
+    email: Joi.string().pattern(emailRegexp).required(),
+})
+
 const User = model('user', userSchema);
 
 const schemas = {
     signup: registerJoiSchema,
+    verify: verifyEmailschema
 };
 
 module.exports = {
